685ac3df2e310e82a4f6148aded63cf311b4959d
ElijahElrod <elijah.elrod@yahoo.com>
2023-12-20 17:14:05 -0500

    Day 2 Part 2 Completed

--- src/day2/cube_conundrum.rs
+++ src/day2/cube_conundrum.rs
@@ -19,23 +19,21 @@ impl ValidItem for CubeItem <'_> {
 
 
 
-pub fn solve() -> i32 {
+pub fn solve_part_2() -> i32 {
 
     let contents = fs::read_to_string("input-cube.txt").expect("Should have been able to read input file");
     let lines = contents.split("\n").collect::<Vec<&str>>();
 
-    let valid_red = CubeItem { qty: 12, color: "red"};
-    let valid_green = CubeItem { qty: 13, color: "green"};
-    let valid_blue = CubeItem { qty: 14, color: "blue"};
-
     let mut game_id_sum = 0;
    
     for line in lines.iter() {
-         let mut valid_game: bool = true;
         let id_marker = line.find(":").expect("Couldn't find ID Marker : ");
-        let game_id = line.substring(4, id_marker);
+
         let game_contents = line.substring(id_marker+1, line.len());
         let sub_groups = game_contents.split(";").collect::<Vec<&str>>(); // (3 blue, 4 red)
+        let mut min_green = 0;
+        let mut min_red = 0;
+        let mut min_blue = 0;
 
         for group in sub_groups.iter() {
             let items = group.split(",").collect::<Vec<&str>>();
@@ -43,16 +41,29 @@ pub fn solve() -> i32 {
                
                 let item_details = item.trim().split(" ").collect::<Vec<&str>>();     
                 let curr_item = CubeItem { qty: item_details[0].parse::<i32>().unwrap(), color: item_details[1]};
-                if !curr_item.is_item_valid(&valid_blue) && !curr_item.is_item_valid(&valid_green) && !curr_item.is_item_valid(&valid_red) {                        
-                    valid_game = false;
-                    break
+                match curr_item.color {
+                    "red" => {
+                        if curr_item.qty > min_red {
+                            min_red = curr_item.qty;
+                        }
+                    },
+                    "blue" => {
+                        if curr_item.qty > min_blue {
+                            min_blue = curr_item.qty;
+                        }
+                    },
+                    "green" => {
+                        if curr_item.qty > min_green {
+                            min_green = curr_item.qty;
+                        }
+                    }
+                    _ => continue
                 }
             }
         }
 
-        if valid_game {
-            game_id_sum += game_id.trim().parse::<i32>().unwrap();
-        }
+       
+       game_id_sum += min_blue * min_green * min_red
     }
 
 
